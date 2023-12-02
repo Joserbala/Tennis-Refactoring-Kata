f9edf06305b420029f84ca6c5f474377450b527d
Joser Gala Naranjo <jgalanar@gmail.com>
2023-12-02 17:51:52 +0100

    R(tennis5): remove duplication

--- csharp/Tennis/TennisGame5.cs
+++ csharp/Tennis/TennisGame5.cs
@@ -39,6 +39,8 @@ namespace Tennis
 
             if (HasWon(p1, p2))
                 return $"Win for {player1Name}";
+            else if(HasWon(p2, p1))
+                return $"Win for {player2Name}";
 
             return (p1, p2) switch
             {
@@ -46,25 +48,19 @@ namespace Tennis
                 (0, 1) => "Love-Fifteen",
                 (0, 2) => "Love-Thirty",
                 (0, 3) => "Love-Forty",
-                (0, 4) => $"Win for {player2Name}",
                 (1, 0) => "Fifteen-Love",
                 (1, 1) => "Fifteen-All",
                 (1, 2) => "Fifteen-Thirty",
                 (1, 3) => "Fifteen-Forty",
-                (1, 4) => $"Win for {player2Name}",
                 (2, 0) => "Thirty-Love",
                 (2, 1) => "Thirty-Fifteen",
                 (2, 2) => "Thirty-All",
                 (2, 3) => "Thirty-Forty",
-                (2, 4) => $"Win for {player2Name}",
                 (3, 0) => "Forty-Love",
                 (3, 1) => "Forty-Fifteen",
                 (3, 2) => "Forty-Thirty",
                 (3, 3) => "Deuce",
                 (3, 4) => $"Advantage {player2Name}",
-                (4, 0) => $"Win for {player1Name}",
-                (4, 1) => $"Win for {player1Name}",
-                (4, 2) => $"Win for {player1Name}",
                 (4, 3) => $"Advantage {player1Name}",
                 (4, 4) => "Deuce",
                 _ => throw new ArgumentException("Invalid score.")
